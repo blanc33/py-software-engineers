6e6265ddb6b10412569926784bb7215903dc49f7
Liubov Kobylianska <blondblanc3@gmail.com>
2026-02-17 22:33:55 -0500

    Solution0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+class SoftwareEngineer:  # parent for all other classes
+    def __init__(self, name: str) -> None:  # como usar name
+        self.name = name
+        self.skills = []  # extended by the child classes
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
